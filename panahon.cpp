aa3b687c757dd91e12fd008ac9bdfbe62e0d6be8
Frietz Osorio <frietz_osorio@yahoo.com>
2010-11-16 10:24:03 +0800

    add GPL license

--- panahon.cpp
+++ panahon.cpp
@@ -1,3 +1,23 @@
+/*
+ * Panahon is a simple weather application
+ * Copyright (C) 2010 Frietz Osorio
+ *
+ * This file is part of Panahon.
+ *
+ * Panahon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Panahon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Panahon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <QtGui>
 #include "panahon.h"
 #include "google_map.h"
